7c992221a38fa3378852ab1269da82be31713b63
Giorgi Koroshinadze <gkoros@brokers.io>
2020-08-13 19:09:43 +0400

    initial commit

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('config');
+const bodyParser = require('body-parser');
+
+// Load the auth module
+const register = require('./register.js');
+const authenticate = require('./authenticate.js');
+const authorize = require('./middleware/authorize.js');
+
+// Configure Express
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var corsOrigin = config.get('cors-origin');
+if (corsOrigin === '*') {
+  app.use(cors());
+} else {
+  app.use(cors({ origin: corsOrigin }));
+}
+
+// Load custom middleware
+const injectRequestId = require('./middleware/injectRequestId.js');
+const setLogging = require('./middleware/setLogging.js');
+app.use(injectRequestId);
+app.use(setLogging);
+
+// Registration route
+app.post(config.get('registrationRoute'), register, function (req, res) {
+  if (req.body.identifier && req.body.password && req.body.repeatPassword) {
+    req.log(`Attempting to register user with ID '${req.body.identifier}'.`);
+  } else {
+    req.log('Registration request failed because of missing credentials.');
+    res.status(400).json({ message: 'Request missing login credentials.' });
+  }
+});
+
+// Authentication route
+app.post(config.get('authenticationRoute'), function (req, res) {
+  if (req.body.identifier && req.body.password) {
+    req.log(`Attempting to authenticate user with ID '${req.body.identifier}'.`);
+    let token = null;
+    try {
+      token = authenticate(req.body.identifier, req.body.password);
+      req.log(`Authentication of user with ID '${req.body.identifier}' successful! Token: ${token}`);
+      res.json({ message: 'Login successful!', token: token });
+    } catch (error) {
+      req.log(`Authentication of user with ID '${req.body.identifier}' failed.`);
+      res.status(401).end();
+    }
+  } else {
+    req.log('Authentication request failed because of missing login credentials.');
+    res.status(400).json({ message: 'Request missing login credentials.' });
+  }
+});
+
+// Authorization route
+app.get(config.get('authorizationRoute'), authorize, function (req, res) {
+  res.json({ message: 'Successfully authorized!' });
+});
+
+// Role-specific authorization routes
+var routes = config.get('routes');
+for (var i = 0; i < routes.length; i++) {
+  var route = routes[i];
+  app.get(route.path, authorize, function (req, res, next) {
+    if (req.token[route.role] === true) {
+      res.json({
+        message: `Successfully authorized with role '${route.role}'!`,
+      });
+    } else {
+      res.status(401).end();
+    }
+  });
+}
+
+// 404 - Route not found
+app.use(function (req, res, next) {
+  req.log(`Unknown route '${req.path}' requested.`);
+  res.status(404).json({ message: `Route not found!` });
+});
+
+// Error handling
+app.use(function (err, req, res, _) {
+  req.log(err.stack);
+  res.status(500).json({ message: err.message });
+});
+
+// Start listening for requests
+app.listen(config.get('port'), () => console.log(`Mock auth server listening on port ${config.get('port')}...`));
---
+++ register.js
@@ -0,0 +1,40 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const accounts = require('./accounts.json');
+const fs = require('fs');
+
+// Method that attempts to register a user 
+module.exports = function (req, res, next) {
+  const email = req.body.identifier;
+  const password = req.body.password;
+  const repeatPassword = req.body.repeatPassword;
+  const roles = req.body.roles;
+
+  if(!email || !password || !repeatPassword) throw new Error('Please provide all of the required values!');
+
+  const passwordRegex = /^[A-Za-z]\w{7,15}$/;
+  const emailRegex = /\S+@\S+\.\S+/;
+  const emailExists = accounts.find(o => o.identifier === email);
+
+  if(!email.match(emailRegex)) throw new Error('INVALID_EMAIL.');
+
+  if(emailExists) throw new Error('EMAIL_IS_ALREADY_USED'); 
+
+  // TODO: if(!password.match(passwordRegex)) throw new Error('INVALID_PASSWORD. Must be 8 - 16 characters, must start with letter and contain at least one number.');
+
+  if(repeatPassword !== password) throw new Error('PASSWORDS_DO_NOT_MATCH.');
+
+  fs.readFile('accounts.json', 'utf8', function readFileCallback(err, data){
+    if (err){
+        throw new Error (err);
+    } else {
+    obj = JSON.parse(data);
+    obj.push({identifier: email, password, roles});
+    json = JSON.stringify(obj);
+    
+    fs.writeFile('accounts.json', json, 'utf8', () => { 
+      res.status(200).end();
+      console.log(obj);
+    });
+  }});
+};
